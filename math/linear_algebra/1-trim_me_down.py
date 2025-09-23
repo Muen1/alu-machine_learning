65114405973ede89c788385084ee13cc349f8031
Muen1 <c.mutie@gmail.com>
2025-09-23 07:31:14 +0200

    fixed errors

--- math/linear_algebra/1-trim_me_down.py
+++ math/linear_algebra/1-trim_me_down.py
@@ -5,3 +5,4 @@ for row in matrix:
     the_middle.append(row[2:4])
 print("The middle columns of the matrix are: {}".format(the_middle))
 
+
--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -5,3 +5,4 @@ def matrix_shape(matrix):
         shape.append(len(matrix))
         matrix = matrix[0] if matrix else None
     return shape
+
